fa542dbb3ecda1dc00c1a71109eccb85a136cd91
NoorHamid <nhamid15@gmail.com>
2020-05-05 23:24:58 -0700

    add songs to playlists

--- spotify_tool.py
+++ spotify_tool.py
@@ -1,34 +1,13 @@
 import sys
 import spotipy
 import spotipy.util as util
-import dotenv
-import json
 
-dotenv.load_dotenv()
-
-# spotify creds
-spotify_user = ""
+# Global variable(s)
 # include scopes for both public and private??
-spotify_access_scope = 'playlist-modify-public'
+SPOTIFY_ACCESS_SCOPE = 'playlist-modify-private'
 
-token = util.prompt_for_user_token(spotify_user, spotify_access_scope)
-
-if not token:
-    exit(1)
-
-# print user playlists
-sp = spotipy.Spotify(auth=token)
-playlists = sp.current_user_playlists(20,0)["items"]
-# print(json.dumps(playlist, indent=2))
-
-playlist_names = [x["name"] for x in playlists]
-print(playlist_names)
-
-# add tracks to playlist
-#print("Number of arguments ", len(sys.argv))
-# print("The arguments are: ", str(sys.argv))
+# Get arguments
 print("This is sys.argv: ", sys.argv[0])
-
 if len(sys.argv) > 3:
     username = sys.argv[1]
     playlist_id = sys.argv[2]
@@ -37,9 +16,25 @@ else:
     print("Usage: %s username playlist_id track_id ..." % (sys.argv[0],))
     sys.exit()
 
-scope = 'playlist-modify-public'
-token = util.prompt_for_user_token(username, scope)
+# Get token
+token = util.prompt_for_user_token(username, SPOTIFY_ACCESS_SCOPE)
 
+# Query playlists
+playlists = None
+if token:
+    sp = spotipy.Spotify(auth=token)
+    playlists = sp.current_user_playlists(20, 0)["items"]
+else:
+    print(f"Can't get token for user {username}!")
+
+# Print playlists
+if playlists:
+    playlist_names = [x["name"] for x in playlists]
+    print(playlist_names)
+else:
+    print('No playlists found!')
+
+# Add tracks to playlist
 if token:
     sp = spotipy.Spotify(auth=token)
     sp.trace = False
