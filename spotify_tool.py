93608df5450428606e01e01b49d0dc81f88864d4
NoorHamid <nhamid15@gmail.com>
2020-05-12 16:51:49 -0700

    args debug error

--- spotify_tool.py
+++ spotify_tool.py
@@ -1,44 +1,68 @@
 import sys
 import spotipy
 import spotipy.util as util
+import argparse
 
 # Global variable(s)
 # include scopes for both public and private??
 SPOTIFY_ACCESS_SCOPE = 'playlist-modify-private'
 
-# Get arguments
-print("This is sys.argv: ", sys.argv[0])
-if len(sys.argv) > 3:
-    username = sys.argv[1]
-    playlist_id = sys.argv[2]
-    track_ids = sys.argv[3:]
-else:
-    print("Usage: %s username playlist_id track_id ..." % (sys.argv[0],))
-    sys.exit()
 
-# Get token
-token = util.prompt_for_user_token(username, SPOTIFY_ACCESS_SCOPE)
+def parse_args():
+    parser = argparse.ArgumentParser(description='This spotify add will add a song to a user playlist or multiple playlists')
 
-# Query playlists
-playlists = None
-if token:
-    sp = spotipy.Spotify(auth=token)
-    playlists = sp.current_user_playlists(20, 0)["items"]
-else:
-    print(f"Can't get token for user {username}!")
+    parser.add_argument('--username', '-u', required=True, type=str,
+                        help='x')
+    parser.add_argument('--playlist_ids', '-p', required=True, type=str,
+                        help='x', action = 'append')
+    parser.add_argument('--track_ids', '-t', metavar='STR', required=True, type=str,
+                        help='x', action = 'append')
+    args = parser.parse_args()
+    return args
 
-# Print playlists
-if playlists:
-    playlist_names = [x["name"] for x in playlists]
-    print(playlist_names)
-else:
-    print('No playlists found!')
+#-- name -x = shorthand name
+#metavar = variable.
+#req = need config file if true
+#type = Path what converts cmd line into.
 
-# Add tracks to playlist
-if token:
-    sp = spotipy.Spotify(auth=token)
-    sp.trace = False
-    results = sp.user_playlist_add_tracks(username, playlist_id, track_ids)
-    print(results)
-else:
-    print("Can't get token for", username)
+
+
+
+def main():
+    args = parse_args()
+    username = args.username
+    playlist_ids = args.playlist_ids
+    track_ids = args.track_ids
+
+    # Get token
+    token = util.prompt_for_user_token(username, SPOTIFY_ACCESS_SCOPE)
+
+    # Query playlists
+    playlists = None
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        playlists = sp.current_user_playlists(20, 0)["items"]
+    else:
+        print(f"Can't get token for user {username}!")
+
+    # Print playlists
+    if playlists:
+        playlist_names = [x["name"] for x in playlists]
+        print(playlist_names)
+    else:
+        print('No playlists found!')
+
+    # Add tracks to playlist
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False
+        results = sp.user_playlist_add_tracks(username, playlist_ids, track_ids)
+        print(results)
+    else:
+        print("Can't get token for", username)
+
+if __name__ == '__main__':
+    main()
+
+
+
