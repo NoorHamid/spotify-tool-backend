d8bd9d2248f3795e9735491197a1f302b5b2f8e8
NoorHamid <nhamid15@gmail.com>
2020-05-18 00:41:10 -0700

    web api

--- app.py
+++ app.py
@@ -2,29 +2,35 @@ import sys
 import json
 import spotipy
 import dotenv
+
 dotenv.load_dotenv()
 import spotipy.util as util
 
-
-SPOTIFY_ACCESS_SCOPE = 'playlist-modify-private'
+SPOTIFY_ACCESS_SCOPE = "playlist-modify-private"
 
 from flask import Flask
 from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
-@app.route('/name/<user>' )
+    return "Hello, World!"
+
+
+@app.route("/name/<user>")
 def hello_user(user):
-    return f'hello {user}!'
+    return f"hello {user}!"
+
 
 TOKEN = None
+
 
 @app.before_request
 def get_login():
     # Get
-    username = request.headers.get('username')
+    username = request.headers.get("username")
     global TOKEN
     TOKEN = util.prompt_for_user_token(username, SPOTIFY_ACCESS_SCOPE)
 
@@ -32,30 +38,49 @@ def get_login():
         raise Exception("barf no token")
 
 
-@app.route('/library', methods=["POST"])
+@app.route("/library", methods=["POST"])
 def get_music():
-        playlist_ids = request.json['playlist-ids']
-        track_ids = request.json['track-ids']
-        username = request.json['username']
+    playlist_ids = request.json["playlist-ids"]
+    track_ids = request.json["track-ids"]
+    username = request.json["username"]
 
-        # Query playlists
-        sp = spotipy.Spotify(auth=token)
-        playlists = sp.current_user_playlists(20, 0)["items"]
+    # Query playlists
+    sp = spotipy.Spotify(auth=token)
+    playlists = sp.current_user_playlists(20, 0)["items"]
 
-        # Add tracks to playlist
-        sp = spotipy.Spotify(auth=token)
-        sp.trace = False
-        for playlist in playlist_ids:
-            results = sp.user_playlist_add_tracks(username, playlist, track_ids)
-            print(results)
+    # Add tracks to playlist
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    for playlist in playlist_ids:
+        results = sp.user_playlist_add_tracks(username, playlist, track_ids)
+        print(results)
 
-        return "lol"
+    return "lol"
 
-@app.route('/playlists', methods=["GET"])
+
+# Print playlists
+@app.route("/playlists", methods=["GET"])
 def list_playlists():
-    #Print playlists
     sp = spotipy.Spotify(auth=TOKEN)
     playlists = sp.current_user_playlists(20, 0)["items"]
-    names = [{'name': x['name'], 'uri':x['uri']} for x in playlists]
+    names = [{"name": x["name"], "uri": x["uri"]} for x in playlists]
     print(playlists)
     return json.dumps(names)
+
+
+# Query Search
+@app.route("/tracks", methods=["GET"])
+def search_songs():
+    q = request.args["search"]
+    sp = spotipy.Spotify(auth=TOKEN)
+    response = sp.search(q)
+    song_results = []
+    for song in response["tracks"]["items"]:
+        song_obj = {
+            "name": song["name"],
+            "uri": song["uri"],
+            "artist": song["artists"][0]["name"],
+            "image": song["album"]["images"][0]["url"],
+        }
+        song_results.append(song_obj)
+    return json.dumps(song_results)
